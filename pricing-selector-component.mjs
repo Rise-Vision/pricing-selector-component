ee1bad2aaa4ff59fc031d20a27e9aeb3d3ad5a63
Tyler Johnson <mail@tyler-johnson.ca>
2019-05-27 17:14:47 -0400

    Respond to slider and input box

--- pricing-selector-component.mjs
+++ pricing-selector-component.mjs
@@ -3,90 +3,85 @@ import {PolymerElement, html} from "https://unpkg.com/@polymer/polymer@next/poly
 class PricingSelectorComponent extends PolymerElement {
   static get properties() {
     return {
-      showDisplayPrompt: {
-        type: Boolean,
-        value: false,
-      },
-      displayPrompt: {
-        type: String,
-        value: "How many Displays do you want?",
-      },
-      displayCount: {
-        type: Number,
-        value: 2,
-        reflectToAttribute: true,
-        notify: true
-      },
-      showDiscountPrompt: {
-        type: Boolean,
-        value: false,
-      },
-      discountPrompt: {
-        type: String,
-        value: "Are you a school or a non-profit?",
-      },
-      discountPromptYesText: {
-        type: String,
-        value: "Yes (get a 10% discount)",
-      },
-      discountPromptNoText: {
-        type: String,
-        value: "No",
-      },
-      showPeriodToggle: {
-        type: Boolean,
-        value: false,
-      },
-      periodYearlyText: {
-        type: String,
-        value: "I want to pay yearly",
-      },
-      periodMonthlyText: {
-        type: String,
-        value: "I want to pay monthly",
-      }
+      showDisplayPrompt: {type: Boolean, value: false},
+      displayPrompt: {type: String, value: "How many Displays do you want?"},
+      displayCount: {type: Number, value: 2, reflectToAttribute: true, notify: true},
+      showCountBox: {type: Boolean, value: false},
+      showDiscountPrompt: {type: Boolean, value: false},
+      discountPrompt: {type: String, value: "Are you a school or a non-profit?"},
+      discountPromptYesText: {type: String, value: "Yes (get a 10% discount)"},
+      discountPromptNoText: {type: String, value: "No"},
+      showPeriodToggle: {type: Boolean, value: false},
+      periodYearlyText: {type: String, value: "I want to pay yearly"},
+      periodMonthlyText: {type: String, value: "I want to pay monthly"}
     };
   }
 
   updateDisplayCount() {
-    this.set("displayCount", this.shadowRoot.getElementById("displaySlider").value);
+    const sliderCount = this.shadowRoot.getElementById("displayCountSlider").value;
+
+    if (sliderCount === "100") {
+      const boxCount = parseInt(this.shadowRoot.getElementById("displayCountBox").value) || 100;
+      this.set("displayCount", Math.max(boxCount, 100));
+      this.shadowRoot.getElementById("displayCountBox").value = this.displayCount;
+    } else {
+      this.set("displayCount", sliderCount);
+    }
+  }
+
+  updateCountBox() {
+    this.updateDisplayCount();
+    this.updateCountBoxVisibility();
+  }
+
+  updateCountBoxVisibility() {
+    this.set("showCountBox", parseInt(this.displayCount) >= 100);
   }
 
   static get template() {
     return html`
       <style>
-        :host {
-          display: inline-block;
+        #main {
+          width: 18em;
+          text-align: center;
+        }
+        #promptText {
+          font-weight: bold;
+        }
+        #displayCountBox {
+          text-align: center;
+          width: 4em;
+          margin: 0.5em 0;
         }
         input[type=range] {
           -webkit-appearance: none;
           height: 12px;
           width: 100%;
           margin: 0;
+          padding-top: 0.5em;
         }
         input[type=range]::-webkit-slider-thumb {
           -webkit-appearance: none;
-          border: 1px solid #000000;
           height: 20px;
           width: 20px;
           border-radius: 50%;
           background: black;
-          cursor: pointer;
           margin-top: -5;
         }
         input[type=range]::-webkit-slider-runnable-track {
           width: 100%;
           height: 10px;
-          cursor: pointer;
           background: #e8e8e8;
           border-radius: 3px;
-          border: 0px solid #010101;
         }
       </style>
-      <div id="displayPrompt" hidden=[[!showDisplayPrompt]]>
-        <div>[[displayPrompt]]</div>
-        <div id="displayCount">[[displayCount]]</div>
-        <input id="displaySlider" on-input="updateDisplayCount" type="range" min="1" max="99" value="{{displayCount}}">
+      <div id="main">
+        <div id="displayPrompt" hidden=[[!showDisplayPrompt]]>
+          <div id="promptText">[[displayPrompt]]</div>
+          <div id="displayCountText" hidden=[[showCountBox]]>[[displayCount]]</div>
+          <input type="text" id="displayCountBox" on-change="updateCountBox" hidden=[[!showCountBox]] value=[[displayCount]] />
+          <input id="displayCountSlider" on-input="updateDisplayCount" on-change="updateCountBoxVisibility" type="range" value="{{displayCount}}">
+        </div>
       </div>
     `;
   }
