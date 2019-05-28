d87b3dd2555ed0a1aa3f6ae277a70194be117061
Tyler Johnson <mail@tyler-johnson.ca>
2019-05-27 21:07:30 -0400

    Add discount prompt section

--- pricing-selector-component.mjs
+++ pricing-selector-component.mjs
@@ -3,14 +3,15 @@ import {PolymerElement, html} from "https://unpkg.com/@polymer/polymer@next/poly
 class PricingSelectorComponent extends PolymerElement {
   static get properties() {
     return {
-      showDisplayPrompt: {type: Boolean, value: false},
-      displayPrompt: {type: String, value: "How many Displays do you want?"},
+      showDisplayCountSection: {type: Boolean, value: false},
+      displayCountText: {type: String, value: "How many Displays do you want?"},
       displayCount: {type: Number, value: 2, reflectToAttribute: true, notify: true},
       showCountBox: {type: Boolean, value: false},
-      showDiscountPrompt: {type: Boolean, value: false},
+      showDiscountSection: {type: Boolean, value: false},
+      applyDiscount: {type: Boolean, value: false},
       discountPrompt: {type: String, value: "Are you a school or a non-profit?"},
-      discountPromptYesText: {type: String, value: "Yes (get a 10% discount)"},
-      discountPromptNoText: {type: String, value: "No"},
+      discountPromptYesText: {type: String, value: "(get a 10% discount)"},
+      discountPromptNoText: {type: String, value: ""},
       showPeriodToggle: {type: Boolean, value: false},
       periodYearlyText: {type: String, value: "I want to pay yearly"},
       periodMonthlyText: {type: String, value: "I want to pay monthly"}
@@ -38,15 +39,50 @@ class PricingSelectorComponent extends PolymerElement {
     this.set("showCountBox", parseInt(this.displayCount) >= 100);
   }
 
+  discountYes() {
+    this.applyDiscount = true;
+  }
+
+  discountNo() {
+    this.applyDiscount = false;
+  }
+
   static get template() {
     return html`
       <style>
         #main {
-          width: 18em;
+          width: 25em;
           text-align: center;
         }
-        #promptText {
+        section {
+          margin-bottom: 2em;
+        }
+        #discountContainer {
+          display: flex;
+          align-items: center;
+          border: solid 1px #292b2c;
+          border-radius: 4px;
+          width: 100%
+        }
+        .discountOption {
+          width: 50%;
+        }
+        .discountOption span {
+          font-size: small;
+        }
+        .discountOption[selected] {
+          background-color: #e8e8e8;
+        }
+        .discountOption[selected]::after {
+          content: "\\002714";
+          margin: 0.5em;
+        }
+        .promptText {
           font-weight: bold;
+          margin-bottom: 0.5em;
+        }
+        #displayCountSlider {
+          outline: none
         }
         #displayCountBox {
           text-align: center;
@@ -76,12 +112,20 @@ class PricingSelectorComponent extends PolymerElement {
         }
       </style>
       <div id="main">
-        <div id="displayPrompt" hidden=[[!showDisplayPrompt]]>
-          <div id="promptText">[[displayPrompt]]</div>
+        <section id="displayCountSection" hidden=[[!showDisplayCountSection]]>
+          <div class="promptText">[[displayCountText]]</div>
           <div id="displayCountText" hidden=[[showCountBox]]>[[displayCount]]</div>
           <input type="text" id="displayCountBox" on-change="updateCountBox" hidden=[[!showCountBox]] value=[[displayCount]] />
           <input id="displayCountSlider" on-input="updateDisplayCount" on-change="updateCountBoxVisibility" type="range" value="{{displayCount}}">
-        </div>
+        </section>
+
+        <section id="discountSection" hidden=[[!showDiscountSection]]>
+          <div class="promptText">[[discountPrompt]]</div>
+          <div id=discountContainer>
+            <div id="discountYes" on-click="discountYes" class="discountOption" selected$=[[applyDiscount]]>Yes <span>[[discountPromptYesText]]</span></div>
+            <div id="discountNo" on-click="discountNo" class="discountOption" selected$=[[!applyDiscount]]>No [[discountPromptNoText]]</div>
+          </div>
+        </section>
       </div>
     `;
   }
