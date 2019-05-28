0207b11a6a1687aa854685fd79575c9614caba96
Tyler Johnson <mail@tyler-johnson.ca>
2019-05-28 13:44:34 -0400

    Include yearly savings highlight with pricing data

--- pricing-selector-component.mjs
+++ pricing-selector-component.mjs
@@ -12,9 +12,14 @@ class PricingSelectorComponent extends PolymerElement {
       discountPrompt: {type: String, value: "Are you a school or a non-profit?"},
       discountPromptYesText: {type: String, value: "(get a 10% discount)"},
       discountPromptNoText: {type: String, value: ""},
-      showPeriodToggle: {type: Boolean, value: false},
+      showPeriodSection: {type: Boolean, value: false},
       periodYearlyText: {type: String, value: "I want to pay yearly"},
-      periodMonthlyText: {type: String, value: "I want to pay monthly"}
+      periodMonthlyText: {type: String, value: "I want to pay monthly"},
+      period: {type: String, value: "yearly", reflectToAttribute: true, notify: true},
+      periodYearly: {type: Boolean, computed: "isYearly(period)"},
+      periodMonthly: {type: Boolean, computed: "isMonthly(period)"},
+      pricingData: {type: Object, value: {}},
+      yearlySavings: {type: String, computed: "getYearlySavings(pricingData)"}
     };
   }
 
@@ -47,6 +52,44 @@ class PricingSelectorComponent extends PolymerElement {
     this.applyDiscount = false;
   }
 
+  setYearly() {
+    this.period = "yearly";
+  }
+
+  setMonthly() {
+    this.period = "monthly";
+  }
+
+  isYearly(period) {return period === "yearly"}
+  isMonthly(period) {return period === "monthly"}
+  getYearlySavings(pricingData) {
+    if (Object.keys(pricingData).length === 0) {return "";}
+
+    const monthlyPlan = pricingData.filter(plan=>{
+      return plan.period === 1 && plan.period_unit === "month" && plan.currency_code === "USD";
+    })[0];
+
+    const yearlyPlan = pricingData.filter(plan=>{
+      return plan.period === 1 && plan.period_unit === "year" && plan.currency_code === "USD";
+    })[0];
+
+    if (!monthlyPlan || !yearlyPlan) {return "";}
+
+    const monthlyPrice = monthlyPlan.tiers.filter(tier=>{
+      const upperPrice = tier.ending_unit ? tier.ending_unit : Number.MAX_SAFE_INTEGER;
+      return tier.starting_unit <= this.displayCount && tier.ending_unit >= this.displayCount;
+    })[0].price;
+
+    const yearlyPrice = yearlyPlan.tiers.filter(tier=>{
+      const upperPrice = tier.ending_unit ? tier.ending_unit : Number.MAX_SAFE_INTEGER;
+      return tier.starting_unit <= this.displayCount && tier.ending_unit >= this.displayCount;
+    })[0].price;
+
+    const savings = (monthlyPrice * 12) - yearlyPrice;
+
+    return `Save $${savings} every year!`;
+  }
+
   static get template() {
     return html`
       <style>
@@ -57,9 +100,9 @@ class PricingSelectorComponent extends PolymerElement {
         section {
           margin-bottom: 2em;
         }
-        #discountContainer {
+        .toggleContainer {
           display: flex;
-          align-items: center;
+          align-items: stretch;
           border: solid 1px #292b2c;
           border-radius: 4px;
           width: 100%
@@ -113,6 +156,7 @@ class PricingSelectorComponent extends PolymerElement {
         }
       </style>
       <div id="main">
+
         <section id="displayCountSection" hidden=[[!showDisplayCountSection]]>
           <div class="promptText">[[displayCountText]]</div>
           <div id="displayCountText" hidden=[[showCountBox]]>[[displayCount]]</div>
@@ -122,9 +166,16 @@ class PricingSelectorComponent extends PolymerElement {
 
         <section id="discountSection" hidden=[[!showDiscountSection]]>
           <div class="promptText">[[discountPrompt]]</div>
-          <div id=discountContainer>
+          <div id=discountContainer class="toggleContainer">
             <div id="discountYes" on-click="discountYes" class="discountOption" selected$=[[applyDiscount]]>Yes <span>[[discountPromptYesText]]</span></div>
             <div id="discountNo" on-click="discountNo" class="discountOption" selected$=[[!applyDiscount]]>No [[discountPromptNoText]]</div>
+          </div>
+        </section>
+
+        <section id="periodSection" hidden=[[!showPeriodSection]]>
+          <div id=periodContainer class="toggleContainer">
+            <div id="periodYearly" on-click="setYearly" class="discountOption" selected$=[[periodYearly]]>[[periodYearlyText]] <span>[[yearlySavings]]</span></div>
+            <div id="periodMonthly" on-click="setMonthly" class="discountOption" selected$=[[periodMonthly]]><div>[[periodMonthlyText]]</div></div>
           </div>
         </section>
       </div>
---
+++ test/pricing-data.mjs
@@ -0,0 +1,481 @@
+export const pricingData = [
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-cdn05y",
+    "name": "Digital Displays 5 Year Plan Cdn",
+    "period": 5,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558483487,
+    "giftable": false,
+    "resource_version": 1558483487756,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "CAD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 34650,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 39600,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 44550,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 49500,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-cad03y",
+    "name": "Digital Displays 3 Year Plan Cdn",
+    "invoice_name": "Digital Displays 3 Year Plan Cdn",
+    "period": 3,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558483654,
+    "giftable": false,
+    "resource_version": 1558483654238,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "CAD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 20790,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 23760,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 26730,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 29700,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-cad02y",
+    "name": "Digital Displays 2 Year Plan Cdn",
+    "invoice_name": "Digital Displays 2 Year Plan Cdn",
+    "period": 2,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558483751,
+    "giftable": false,
+    "resource_version": 1558483751626,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "CAD",
+    "tiers": [
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 19800,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 17820,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 15840,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 12,
+        "price": 13860,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-cad01y",
+    "name": "Digital Displays Annual Plan Cdn",
+    "invoice_name": "Digital Displays Annual Plan Cdn",
+    "period": 1,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558483882,
+    "giftable": false,
+    "resource_version": 1558483882113,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "CAD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 7700,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 8800,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 9900,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 11000,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-cad01m",
+    "name": "Digital Displays Monthly Plan Cdn",
+    "invoice_name": "Digital Displays Monthly Plan Cdn",
+    "period": 1,
+    "period_unit": "month",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558483972,
+    "giftable": false,
+    "resource_version": 1558483972486,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "CAD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 700,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 800,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 900,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 1000,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-usd05y",
+    "name": "Digital Displays 5 year plan",
+    "invoice_name": "Digital Displays 5 year plan",
+    "period": 5,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558484065,
+    "giftable": false,
+    "resource_version": 1558484065618,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "USD",
+    "tiers": [
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 49500,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 44550,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 39600,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 12,
+        "price": 34650,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-usd03y",
+    "name": "Digital Displays 3 year plan",
+    "invoice_name": "Digital Displays 3 year plan",
+    "period": 3,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558484151,
+    "giftable": false,
+    "resource_version": 1558484151547,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "USD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 20790,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 23760,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 26730,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 29700,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-usd02y",
+    "name": "Digital Displays 2 year plan",
+    "invoice_name": "Digital Displays 2 year plan",
+    "period": 2,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558484235,
+    "giftable": false,
+    "resource_version": 1558484235413,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "USD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 13860,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 15840,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 17820,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 19800,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-usd01y",
+    "name": "Digital Displays Annual Plan",
+    "invoice_name": "Digital Displays Annual Plan",
+    "period": 1,
+    "period_unit": "year",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558484314,
+    "giftable": false,
+    "resource_version": 1558484314106,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "USD",
+    "tiers": [
+      {
+        "starting_unit": 12,
+        "price": 7700,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 8800,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 9900,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 11000,
+        "object": "tier"
+      }
+    ]
+  },
+  {
+    "id": "34e8b511c4cc4c2affa68205cd1faaab427657dc-usd01m",
+    "name": "Digital Displays Monthly Plan",
+    "invoice_name": "Digital Displays Monthly Plan",
+    "period": 1,
+    "period_unit": "month",
+    "pricing_model": "volume",
+    "free_quantity": 0,
+    "status": "active",
+    "enabled_in_hosted_pages": true,
+    "enabled_in_portal": true,
+    "addon_applicability": "all",
+    "tax_code": "SW052000",
+    "accounting_code": "4112",
+    "is_shippable": false,
+    "updated_at": 1558484542,
+    "giftable": false,
+    "resource_version": 1558484542319,
+    "object": "plan",
+    "charge_model": "volume",
+    "taxable": true,
+    "currency_code": "USD",
+    "tiers": [
+      {
+        "starting_unit": 1,
+        "ending_unit": 1,
+        "price": 1000,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 2,
+        "ending_unit": 3,
+        "price": 900,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 4,
+        "ending_unit": 11,
+        "price": 800,
+        "object": "tier"
+      },
+      {
+        "starting_unit": 12,
+        "price": 700,
+        "object": "tier"
+      }
+    ]
+  }
+]
